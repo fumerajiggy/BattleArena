065ebfecbbfd330d512e051ee5c6e24c96280f8e
fumerajiggy <fumerajiggy29@gmail.com>
2026-09-07 16:42:51 +0800

    Intro to Constructor and Properties

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -3,7 +3,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace BattleArena
@@ -12,24 +11,12 @@ namespace BattleArena
     {
         static void Main(string[] args)
         {
-            Warrior Etits = new Warrior();
-            Etits.Name = "Bading";
-            Etits.Health = 300;
-            Etits.AttackPower = 50;
+            Warrior Jiggy = new Warrior("Jiggy", 300, 50);
+            Warrior Orbeast = new Warrior("Orbeast", 10, 1);
+            Warrior Dedong = new Warrior("Dedong", 100, 20);
 
-            Warrior Orbeast = new Warrior();
-            Orbeast.Name = "Orbeast";
-            Orbeast.Health = 10;
-            Orbeast.AttackPower = 1;
-
-            Warrior Dedong = new Warrior();
-            Dedong.Name = "Dedong";
-            Dedong.Health = 100;
-            Dedong.AttackPower = 20;
-
-
-            Console.WriteLine($"{Etits.Name} has " +
-                $"{Etits.Health} health and {Etits.AttackPower} attack power.");
+            Console.WriteLine($"{Jiggy.Name} has " +
+                $"{Jiggy.Health} health and {Jiggy.AttackPower} attack power.");
             Console.WriteLine("----------------------------------------------");
 
             Console.WriteLine($"{Orbeast.Name} has " +
