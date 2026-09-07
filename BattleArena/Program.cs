b84129873aae07e159169aa382a6777ec626b6ad
fumerajiggy <fumerajiggy29@gmail.com>
2026-09-07 16:49:33 +0800

    Refactor Battle Logic Add Fighter, Marksman and Tank Classes

--- BattleArena/Program.cs
+++ BattleArena/Program.cs
@@ -3,7 +3,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using static BattleArena.Warriors.Marksman;
+
 
 namespace BattleArena
 {
@@ -11,22 +14,23 @@ namespace BattleArena
     {
         static void Main(string[] args)
         {
-            int round = 1;
-            Warrior Jiggy = new Warrior("Jiggy", 300, 15, "Dinilaan");
-            Warrior Orbading = new Warrior("Orbading", 150, 32, "Sinubo");
-            Warrior Dedong = new Warrior("Dedong", 200, 20, "Pinwetan");
+            var Jiggy = new Tank("Jiggy", 300, 15, 5);
+            var Orbeast = new Marksman("Orbeast", 150, 32, 10);
+            var balmond = new Fighter("balmond", 200, 20, 7);
 
             Jiggy.DisplayStatus();
-            Orbading.DisplayStatus();
-            Dedong.DisplayStatus();
+            Orbeast.DisplayStatus();
+            balmond.DisplayStatus();
 
-            while (Jiggy.IsAlive && Orbading.IsAlive)
+            while (Jiggy.IsAlive && Orbeast.IsAlive)
             {
-                Console.WriteLine($"---------------- Round {round}  ----------------");
-                Orbading.Attack(Jiggy);
-                Jiggy.Attack(Orbading);
-                Console.WriteLine("------------------------------------------");
-                round++;
+                Console.WriteLine("\n\n====================================");
+                Jiggy.Attack(Orbeast);
+                Thread.Sleep(1000);
+                Orbeast.Attack(Jiggy);
+                Thread.Sleep(1000);
+                Console.WriteLine("------------------------------------");
+
             }
 
             Console.ReadKey();
